c0c5db86c4231f0e3944f5f70dc22ff3391b1853
Naruth Kongurai <naruth1212@gmail.com>
2018-09-13 00:42:26 -0700

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,65 @@
+{
+    "window.zoomLevel": -1,
+    "editor.detectIndentation": false,
+    "extensions.ignoreRecommendations": false,
+    "editor.wordWrap": "wordWrapColumn",
+    "files.autoSave": "off",
+    "editor.wordWrapColumn": 80,
+    "markdown.extension.preview.autoShowPreviewToSide": false,
+    "markdown.extension.orderedList.marker": "ordered",
+    "workbench.iconTheme": "eq-material-theme-icons-darker",
+    "editor.fontSize": 22,
+    "workbench.colorTheme": "Material Theme Darker High Contrast",
+    "editor.fontFamily": "'Fira Code Light', 'Inconsolata for Powerline'",
+    "editor.tabSize": 2,
+    "editor.letterSpacing": 0.2,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.snippetSuggestions": "top",
+    "editor.formatOnPaste": false,
+    "terminal.integrated.fontSize": 16,
+    "terminal.integrated.cursorBlinking": true,
+    "workbench.sideBar.location": "left",
+    "workbench.statusBar.visible": true,
+    "workbench.activityBar.visible": true,
+    "editor.minimap.enabled": false,
+    "scss.lint.float": "warning",
+    "emmet.excludeLanguages": [],
+    "editor.cursorStyle": "line",
+    "editor.cursorWidth": 3,
+    "editor.renderWhitespace": "none",
+    "editor.fontLigatures": true,
+    "editor.wrappingIndent": "indent",
+    "editor.rulers": [
+      80,
+      100,
+      120
+    ],
+    "workbench.startupEditor": "newUntitledFile",
+    "workbench.statusBar.feedback.visible": false,
+    "workbench.tips.enabled": false,
+    "workbench.editor.tabSizing": "shrink",
+    // "window.title": "${activeEditorShort}${separator}${activeEditorMedium}",
+    "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+    "prettier.semi": false,
+    "prettier.tabWidth": 4,
+    "editor.minimap.renderCharacters": false,
+    "editor.codeLens": false,
+    "editor.colorDecorators": false,
+    "eslint.options": {
+        "parserOptions": {
+            "ecmaVersion": 6,
+            "sourceType": "module",
+            "ecmaFeatures": {
+                "jsx": true,
+                "modules": true
+            }
+        }
+    },
+    "vsicons.dontShowNewVersionMessage": true,
+    "gitlens.advanced.messages": {
+      "suppressShowKeyBindingsNotice": true
+    },
+    "materialTheme.fixIconsRunning": false,
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "explorer.confirmDragAndDrop": false
+  }
